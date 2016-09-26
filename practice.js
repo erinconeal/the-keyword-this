e973a794f26bdda72a149264f63566a05d4ba220
eco <erinconeal@outlook.com>
2016-09-26 15:06:30 -0600

    first commit

--- practice.js
+++ practice.js
@@ -1,19 +1,23 @@
 //We're in a job interview. Answer the following questions (try to not look at your notes unless you have to).
   // 1) What is the purpose of the 'this keyword'?
 
-      //Answer
+      //It depends on how it is called in the function but in general it can't be set by assignment during execution, and it may be different each time the function is called.
 
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
-      //Answer
+      //Explicit (call/apply/bind): Where a function uses the this keyword in its body, its value can be bound to a particular object in the call using the call or apply methods; Calling f.bind(someObject) creates a new function with the same body and scope as f, but where this occurs in the original function, in the new function it is permanently bound to the first argument of bind, regardless of how the function is being used.
+      //Implicit: a function is called as a method of an object: its this is set to the object the method is called on
+      //Default/window: refers to the global object
+      //Constructor(new): When a function is used as a constructor (with the new keyword), its this is bound to the new object being constructed.
 
   // 3) What is the difference between call and apply?
 
-      //Answer
+      //Call: calls a function with a given this value and arguments provided individually.
+      //Apply: The apply() method calls a function with a given this value and arguments provided as an array (or an array-like object).
 
   // 4) What does .bind do?
 
-      //Answer
+      //The bind() method creates a new function that, when called, has its this keyword set to the provided value, with a given sequence of arguments preceding any provided when the new function is called.
 
 
 //Next Problem
@@ -23,16 +27,29 @@
   //email --> which is a string
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
-    //Code Here
-
+var user = {
+  username: "eco",
+  email: "me@me.com",
+  getUsername: function() {
+    return this.username;
+  }
+};
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
-
+user.getUsername();
 
 //Next Problem
 
 
 // Write the function definitions which will make the following function invocations function properly.
-
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.move = 0;
+  this.moveCar = function() {
+    return this.move += 10;
+  }
+}
   //Function Invocations Here
 
 var prius = new Car('Toyota', 'Prius', 2011);
@@ -54,8 +71,8 @@ var getYear = function(){
 //Above you're given the getYear function. Using your prius and mustang objects from above, use the proper syntax that will allow for you to call the getYear function with the prius then the mustang objects being the focal objects. *Don't add getYear as a property on both objects*.
 
 //Note(no tests)
-  //Code Here
-
+getYear.call(prius);
+getYear.call(mustang);
 
 //New Problem
 
@@ -69,14 +86,14 @@ var getMyUsername = function() {
  return this.username;
 };
 
-var userName = getMyUsername(); //Fix this
+var userName = getMyUsername.call(myUser);
 
 //Above you're given an object, and  a function. What will the getUsername function return?
 //Note(no tests)
-  //Answer Here
+  //the myUser username
 
 //In the example above, what is the 'this keyword' bound to when getUsername runs?
 
-  //Answer Here
+  //myUser
 
 //Fix the getMyUsername invocation so that userName will be equal to 'iliketurtles'.
